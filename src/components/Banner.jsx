3b652cbf7474cb91852adfd2fc9131434ad4f2ec
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-09-04 10:25:35 -0500

    Modified to add banner in <header> tag

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,10 @@
 import styles from "./styling.css";
 const Banner = () => {
   return (
-    <div>
+    <header>
       <h1>Orbit Report</h1>
       <p>Click on the buttons to see the satellites in that orbit type</p>
-    </div>
+    </header>
   );
 };
 export default Banner;
